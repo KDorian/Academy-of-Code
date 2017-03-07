6d0f17278aaf5d064d2a1107b8e84e02138301cb
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-03-07 10:11:49 +0100

    Add 2.1_Classes
    Adding Fighter class.

---
+++ 2.1_Classes/src/Fighter.java
@@ -0,0 +1,34 @@
+/**
+ * Created by DK on 07.03.2017.
+ */
+public class Fighter {
+
+    private String name;
+    private int agression;
+    private int strength;
+    private int tactics;
+
+    public Fighter(String name, int agression, int strength, int tactics) {
+        this.name = name;
+        this.agression = agression;
+        this.strength = strength;
+        this.tactics = tactics;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAgression() {
+        return agression;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getTactics() {
+        return tactics;
+    }
+    
+}
---
+++ 2.1_Classes/src/Run.java
@@ -0,0 +1,5 @@
+/**
+ * Created by DK on 07.03.2017.
+ */
+public class Run {
+}
