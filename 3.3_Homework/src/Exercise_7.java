3815fc4c51da44146ba353d497e99cfc598d4cd1
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-03-09 00:50:25 +0100

    Add Exercise_7

---
+++ 3.3_Homework/src/Exercise_7.java
@@ -0,0 +1,8 @@
+/**
+ * Created by DK on 09.03.2017.
+ */
+public class Exercise_7 {
+        public static void main(final String[] args) {
+             System.out.println("\"Mowa jest srebrem, a milczenie złotem.\"");
+            }
+}
