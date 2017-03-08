19bae456663fa761ef1c0fbf618c5eabc4ae662c
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-03-09 00:54:32 +0100

    Add Exercise_8

---
+++ 3.3_Homework/src/Exercise_8.java
@@ -0,0 +1,18 @@
+/**
+ * Created by DK on 09.03.2017.
+ */
+public class Exercise_8 {
+    public static void main(final String[] args) {
+
+        String word1 = "Ala";
+        String word2 = "Sak";
+
+        if (word1.equals(word2)) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+        }
+
+
+    }
+}
