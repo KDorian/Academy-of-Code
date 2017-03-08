041a27b24c26e8df79ba9d34c56600d23ce0ce26
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-03-09 00:47:31 +0100

    Add Exercise_6

---
+++ 3.3_Homework/src/Exercise_6.java
@@ -0,0 +1,11 @@
+/**
+ * Created by DK on 09.03.2017.
+ */
+public class Exercise_6 {
+        public static void main(final String[] args) {
+             String name = "Dorian";
+             String surname = "Kucharski";
+             String sentence = new String(name).concat(" ").concat(surname);
+             System.out.println("Your name and surname is " + sentence);
+            }
+}
